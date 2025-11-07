720271b20dae40b5bb207e2bf88dddb169afa2a6
George Fragiadakis <gfragi@hua.gr>
2025-11-07 15:23:40 +0200

    rearange blocks

--- app/Home.py
+++ app/Home.py
@@ -313,7 +313,7 @@ def _b64_or_empty(path: str) -> str:
         return ""
 
 # point this to your real logo file
-LOGO_PATH = Path(__file__).parent / "assets" / "HUA-Logo-Informatics-Telematics-EN-30-Years-RGB.png"
+LOGO_PATH = Path(__file__).parent / "assets" / "dit_hua_logo.png"
 LOGO_DATA_B64 = _b64_or_empty(str(LOGO_PATH))
 LOGO_DATA_URL = f"data:image/png;base64,{LOGO_DATA_B64}" if LOGO_DATA_B64 else ""
 
